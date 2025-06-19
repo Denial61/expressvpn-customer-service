c286ecf511b105a856421d2527cab67b28541afd
Denial61 <hs51525254@gmail.com>
2025-06-19 12:14:08 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'HitmanPro Customer Service'
+author = 'Denial '
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
